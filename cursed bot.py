37c8d71141462f20a107c97713198fc0786eff4a
MurrayManYT <46605719+MurrayManYT@users.noreply.github.com>
2021-01-13 19:01:47 +0000

    Add files via upload

---
+++ cursed bot.py
@@ -0,0 +1,26 @@
+import discord
+from discord.ext import commands
+from discord.ext.commands import Bot
+client = commands.Bot(command_prefix="UwU ")
+
+@client.event
+async def on_ready():
+    print("bot is ready")
+    
+@client.command()
+async def hewwo(ctx):
+    await ctx.send("hewwo wowwd")
+        
+@client.command()
+async def suicide(ctx):
+    await ctx.send("fucking kill me please help put me out of this misery fucking end my suffering please")
+    
+@client.command()
+async def hewp(ctx):
+    await ctx.send("This is the Help Section of Fuwwy Muwway's Bot. Might be updated. Check github for updates (if muwway adds me to gihub that is uwu).\n```UwU is my prefix.\nUse UwU alongside my commands to trigger them, uwu.``` Might make a list of commands if Muwway is feewing cute uwu. Bye fow now fuwwy fwiends uwu.")
+        
+@client.command()
+async def invite(ctx):
+    await ctx.send("Heres's my invite link: https://discord.com/api/oauth2/authorize?client_id=798553197652344872&permissions=0&scope=bot")
+        
+client.run("Nzk4NTUzMTk3NjUyMzQ0ODcy.X_2sng.SsV05lY_m0brOnBX1NaHyG0KSV0")
